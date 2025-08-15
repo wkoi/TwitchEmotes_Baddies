049d5c33468cd31f917960c1c5d8e56619feaa64
Mekkaman <mekkalovania@gmail.com>
2025-08-15 02:26:23 -0400

    Add files via upload

--- Emotes.lua
+++ Emotes.lua
@@ -1,16 +1,28 @@
 TwitchEmotes_Baddies_Emoticons = {
     ["Crungo"] = "Crungo",
-    ["SputSussy"] = "SputSussy",
+    ["sputSussy"] = "sputSussy",
+    ["OhSnap"] = "OhSnap",
+    ["mhm"] = "mhm",
+    ["GLORPSHIT"] = "GLORPSHIT",
+    ["Shoo"] = "Shoo",
 
 }
 
 TwitchEmotes_Baddies_Emoticons_Pack = {
     ["Crungo"] = "Interface\\AddOns\\TwitchEmotes_Baddies\\emotes\\Crungo.tga:28:28",
-    ["SputSussy"] = "Interface\\AddOns\\TwitchEmotes_Baddies\\emotes\\SputSussy.tga:112:112",
+    ["sputSussy"] = "Interface\\AddOns\\TwitchEmotes_Baddies\\emotes\\sputSussy.tga:32:32",
+    ["OhSnap"] = "Interface\\AddOns\\TwitchEmotes_Baddies\\emotes\\OhSnap.tga:32:32",
+    ["mhm"] = "Interface\\AddOns\\TwitchEmotes_Baddies\\emotes\\mhm.tga:32:32",
+    ["GLORPSHIT"] = "Interface\\AddOns\\TwitchEmotes_Baddies\\emotes\\GLORPSHIT.tga:32:32",
+    ["Shoo"] = "Interface\\AddOns\\TwitchEmotes_Baddies\\emotes\\Shoo.tga:32:32",
 
 }
 
 -- Register animation metadata for animated Moosebrother emotes.
 -- Keep this in sync with the spritesheets generated under /emotes.
 -- glue.tga was generated as a vertical strip of 64 frames, each 32x32, total image height 2048, 15 fps
-    TwitchEmotesBaddies_AddAnimation("Interface\\AddOns\\TwitchEmotes_Baddies\\emotes\\SputSussy.tga", 6, 112, 112, 112, 672, 10)
+    TwitchEmotesBaddies_AddAnimation("Interface\\AddOns\\TwitchEmotes_Baddies\\emotes\\sputSussy.tga", 6, 112, 112, 112, 672, 10)
+    TwitchEmotesBaddies_AddAnimation("Interface\\AddOns\\TwitchEmotes_Baddies\\emotes\\OhSnap.tga", 29, 56, 56, 56, 1624, 12.5)
+    TwitchEmotesBaddies_AddAnimation("Interface\\AddOns\\TwitchEmotes_Baddies\\emotes\\mhm.tga", 9, 74, 64, 74, 576, 5)
+    TwitchEmotesBaddies_AddAnimation("Interface\\AddOns\\TwitchEmotes_Baddies\\emotes\\GLORPSHIT.tga", 75, 92, 64, 92, 4800,33.33)
+    TwitchEmotesBaddies_AddAnimation("Interface\\AddOns\\TwitchEmotes_Baddies\\emotes\\Shoo.tga", 45, 74, 64, 74, 2880, 16)
